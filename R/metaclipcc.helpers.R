f9ac23bdfc177a2a019453ca13b5f00acd0f2b3b
jbedia <joaquin.bedia@gmail.com>
2019-11-07 19:17:31 +0100

    Add observational database lookup table

--- R/metaclipcc.helpers.R
+++ R/metaclipcc.helpers.R
@@ -6,6 +6,7 @@
 #' @return Either a vector with all dataset names (when \code{names.only = TRUE}) or a \code{data.frame} with all the associated metadata.
 #' @keywords internal
 #' @export
+#' @family lookup-tables
 #' @importFrom utils read.csv
 #' @examples showIPCCdatasets()
 
@@ -24,6 +25,7 @@ showIPCCdatasets <- function(names.only = TRUE) {
 #' @return Either a vector with all dataset names (when \code{names.only = TRUE}) or a \code{data.frame} with all the associated metadata.
 #' @keywords internal
 #' @export
+#' @family lookup-tables
 #' @importFrom utils read.csv
 #' @examples showIPCCvars()
 
@@ -31,5 +33,28 @@ showIPCCvars <- function(names.only = TRUE) {
     out <- read.csv(file.path(find.package("metaclipcc"), "var_table.csv"),
                     stringsAsFactors = FALSE, na.strings = "")
     if (isTRUE(names.only)) out <- out$variable
+    return(out)
+}
+
+#' @title Table of Observational reference datasets
+#' @description Shows the internal table with Observtaional Datasets and relevant metadata
+#' @param names.only Logical flag indicating that only the internal dataset names are shown. Default to \code{TRUE}.
+#' Otherwise, the full metadata table is shown
+#' @export
+#' @author J Bedia
+#' @return Either a vector with all dataset names (when \code{names.only = TRUE}) or a \code{data.frame}
+#' with all the associated metadata.
+#' @details All the observational datasets used as reference should be described as
+#' individual instances of the ObservationalDataset Class in the datasource vocabulary.
+#' @keywords internal
+#' @export
+#' @importFrom utils read.csv
+#' @family lookup-tables
+#' @examples showOBSdatasets()
+
+showOBSdatasets <- function(names.only = TRUE) {
+    out <- read.csv(file.path(find.package("metaclipcc"), "obs_dataset_table.csv"),
+                    stringsAsFactors = FALSE, na.strings = "")
+    if (isTRUE(names.only)) out <- out$name
     return(out)
 }
