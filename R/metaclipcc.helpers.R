576d3c098782098c150c3be7208a4b8f238eeb58
jbedia <joaquin.bedia@gmail.com>
2019-11-09 18:19:24 +0100

    Replace GCM and OBS lookup tables by one single dataset table

--- R/metaclipcc.helpers.R
+++ R/metaclipcc.helpers.R
@@ -11,7 +11,7 @@
 #' @examples showIPCCdatasets()
 
 showIPCCdatasets <- function(names.only = TRUE) {
-    out <- read.csv(file.path(find.package("metaclipcc"), "gcm_table.csv"),
+    out <- read.csv(file.path(find.package("metaclipcc"), "dataset_table.csv"),
              stringsAsFactors = FALSE, na.strings = "")
     if (isTRUE(names.only)) out <- out$name
     return(out)
@@ -36,25 +36,11 @@ showIPCCvars <- function(names.only = TRUE) {
     return(out)
 }
 
-#' @title Table of Observational reference datasets
-#' @description Shows the internal table with Observtaional Datasets and relevant metadata
-#' @param names.only Logical flag indicating that only the internal dataset names are shown. Default to \code{TRUE}.
-#' Otherwise, the full metadata table is shown
-#' @export
-#' @author J Bedia
-#' @return Either a vector with all dataset names (when \code{names.only = TRUE}) or a \code{data.frame}
-#' with all the associated metadata.
-#' @details All the observational datasets used as reference should be described as
-#' individual instances of the ObservationalDataset Class in the datasource vocabulary.
-#' @keywords internal
-#' @export
-#' @importFrom utils read.csv
-#' @family lookup-tables
-#' @examples showOBSdatasets()
 
-showOBSdatasets <- function(names.only = TRUE) {
-    out <- read.csv(file.path(find.package("metaclipcc"), "obs_dataset_table.csv"),
-                    stringsAsFactors = FALSE, na.strings = "")
-    if (isTRUE(names.only)) out <- out$name
-    return(out)
-}
+
+# showOBSdatasets <- function(names.only = TRUE) {
+#     out <- read.csv(file.path(find.package("metaclipcc"), "obs_dataset_table.csv"),
+#                     stringsAsFactors = FALSE, na.strings = "")
+#     if (isTRUE(names.only)) out <- out$name
+#     return(out)
+# }
