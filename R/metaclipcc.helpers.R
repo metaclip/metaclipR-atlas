026688aa0a8aff2d3d5972305e6c4fa22dfea526
jbedia <joaquin.bedia@gmail.com>
2020-02-02 23:35:47 +0100

    getFuturePeriod internal helper

--- R/metaclipcc.helpers.R
+++ R/metaclipcc.helpers.R
@@ -37,10 +37,63 @@ showIPCCvars <- function(names.only = TRUE) {
 }
 
 
+#' @title Get future period
+#' @description Get a future period time slice ready to be used by \code{metaclipR} functions
+#' @param project Either \code{"CMIP5"} or \code{"CMIP6"}
+#' @param model GCM name, as encoded in the reference lookup \code{dataset_table.csv}. Type  \code{unique(showIPCCdatasets(names.only = FALSE)$GCM)}
+#' @param future.period Current options include the standard AR5 future time slices for near term, \code{"2021-2040"},
+#'  medium term \code{"2041-2060"} and long term \code{"2081-2100"}, and the global warming levels
+#'  of +1.5 degC \code{"1.5"}, +2 \code{"2"} and +3 \code{"3"}.
+#' @param rcp Experiment. Accepted values are restricted to \code{"historical", "rcp26", "rcp45", "rcp60", "rcp85"}
+#' @return An integer vector of length two, formed by start and end year of the future time slice
+#' @keywords internal
+#' @family lookup-tables
+#' @importFrom utils read.csv
 
-# showOBSdatasets <- function(names.only = TRUE) {
-#     out <- read.csv(file.path(find.package("metaclipcc"), "obs_dataset_table.csv"),
+# #TODO: Handle the way the driving GCM warming level period is retrieved for CORDEX
+# project = "CMIP5"
+# model = ref.model
+# future.period = "1.5"
+# rcp = experiment
+
+metaclipcc.getFuturePeriod <- function(project, model, future.period, rcp) {
+    project <- match.arg(project, choices = c("CMIP5", "CMIP6"))
+    if (future.period == "1.5" | future.period == "2" | future.period == "3" | future.period == "4") {
+        filename <- switch(project,
+                           "CMIP5" = "gwl_CMIP5.csv",
+                           "CMIP6" = "gwl_CMIP6.csv"
+        )
+        out <- read.csv(file.path(find.package("metaclipcc"), filename),
+                        stringsAsFactors = FALSE, na.strings = "")
+        ind.row <- grep(model, out$GCM, fixed = TRUE)
+        ind.col <- grep(paste0(future.period,"_", rcp), names(out), ignore.case = TRUE)
+        yr <- out[ind.row, ind.col]
+        ifelse(yr == 9999, NA, yr)
+        c(yr - 9, yr + 10) %>% return()
+    } else {
+        strsplit(future.period, split = "-") %>% unlist() %>% as.integer() %>% return()
+    }
+}
+
+# metaclipcc.getGWL("CMIP5")
+
+# #' @title Table of IPCC Atlas Regions
+# #' @description Shows the internal table with IPCC regions relevant metadata
+# #' @param names.only Logical flag indicating that only the HorizontalExtent names are shown. Default to \code{TRUE}. Otherwise, the full metadata table is shown
+# #' @export
+# #' @author J Bedia
+# #' @return Either a vector with all HorizontalExtent names (when \code{names.only = TRUE}) or a \code{data.frame} with all the associated metadata.
+# #' @keywords internal
+# #' @export
+# #' @family lookup-tables
+# #' @importFrom utils read.csv
+# #' @examples showIPCCregions(names.only = FALSE)
+#
+# showIPCCregions <- function(names.only = TRUE) {
+#     out <- read.csv(file.path(find.package("metaclipcc"), "regions_table.csv"),
 #                     stringsAsFactors = FALSE, na.strings = "")
-#     if (isTRUE(names.only)) out <- out$name
+#     if (isTRUE(names.only)) out <- out$HorizontalExtent
 #     return(out)
 # }
+
+
