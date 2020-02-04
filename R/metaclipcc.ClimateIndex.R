93577fee57fedd5af32cf66a871e175296699a99
jbedia <joaquin.bedia@gmail.com>
2020-02-04 17:31:37 +0100

    Use a list of graphs as input for multivariable indices

--- R/metaclipcc.ClimateIndex.R
+++ R/metaclipcc.ClimateIndex.R
@@ -1,61 +1,65 @@
 #' @title Encode metadata for IPCC product climate index definition
 #' @description Encode metadata for IPCC product climate index definition
-#' @param graph Input graph
+#' @param graph.list A list of input graphs.
 #' @param index.code Index code
-#' @param new.time.res Temporal resolution of the index. Default to monthly
+#' @param dc.description Optional field to inster dc:description annotations to the
 #' @return A metaclipR object
 #' @details The function asssumes that the index.code passed as argument is an individual instance of the ds:ClimateIndex class,
 #' defined in the ipcc_terms vocabulary
 #' @export
 #' @author J Bedia
-#' @importFrom metaclipR my_add_vertices getNodeIndexbyName randomName knownClassIndividuals
+#' @importFrom metaclipR my_add_vertices my_union_graph getNodeIndexbyName randomName
 #' @importFrom igraph add_edges
 
+metaclipcc.ClimateIndex <- function(graph.list, index.code) {
 
-metaclipcc.ClimateIndex <- function(graph, index.code, new.time.res = "P1M") {
-    if (class(graph$graph) != "igraph") stop("Invalid input graph (not an 'igraph-class' object)")
-    if (is.null(index.code)) {
-        stop("The 'index.code' argument is missing in the argument list, with no default")
-    }
-    ## Hay que arreglar esto!
-    index.choices <- knownClassIndividuals(classname = "ClimateIndex", vocabulary = "ipcc_terms") ## Arreglar esto
-    index.code <- match.arg(index.code,
-                            choices = c("TASMAXNA35", "PRNAP99"))
-    orig.node <- graph$parentnodename
-    graph <- graph$graph
-    cicalc.node <- paste("ClimateIndexCalculation", randomName(), sep = ".")
-    # ClimateIndex calculation node
-    graph <- my_add_vertices(graph,
-                             name = cicalc.node,
-                             label = "ClimateIndexCalculation",
-                             className = "ds:ClimateIndexCalculation")
-    graph <- add_edges(graph,
-                       c(getNodeIndexbyName(graph, orig.node),
-                         getNodeIndexbyName(graph, cicalc.node)),
-                       label = "ds:hadClimateIndexCalculation")
-    # Climate index node
-    nodename <- paste0("ipcc:", index.code)
-    cn <- "ds:ClimateIndex"
+    ref <- showIPCCvars(names.only = FALSE)[grep(index.code, showIPCCvars()),]
+    nodename <- paste0("ClimateIndexCalculation.", randomName())
+    graph <- graph.list[[1]]$graph
     graph <- my_add_vertices(graph,
                              name = nodename,
-                             label = paste("ClimateIndex", index.code, sep = "."),
-                             className = cn)
-    graph <- add_edges(graph,
-                       c(getNodeIndexbyName(graph, cicalc.node),
-                         getNodeIndexbyName(graph, nodename)),
-                       label = "ds:withClimateIndex")
-    # TemporalResolution ---------------------
-    timeres.nodename <- paste("TemporalResolution", randomName(), sep = ".")
-    graph <- my_add_vertices(graph,
-                             name = timeres.nodename,
-                             label = "TemporalResolution",
-                             className = "ds:TemporalResolution",
-                             attr = list("ds:hasTimeStep" = new.time.res))
-    graph <- add_edges(graph,
-                       c(getNodeIndexbyName(graph, cicalc.node),
-                         getNodeIndexbyName(graph, timeres.nodename)),
-                       label = "ds:hasTemporalResolution")
+                             label = "Climate Index Calculation",
+                             className = "ds:ClimateIndexCalculation",
+                             attr = list("dc:description" = paste("Calculation of ", index.code, "Climate Index")))
 
-    return(list("graph" = graph, "parentnodename" = cicalc.node))
+    for (i in 1:length(graph.list)) {
+        if (class(graph.list[[i]]$graph) != "igraph") stop("Invalid input graph (not an 'igraph-class' object)")
+        graph <- my_union_graph(graph, graph.list[[i]]$graph)
+        graph <- add_edges(graph,
+                           c(getNodeIndexbyName(graph, graph.list[[i]]$parentnodename),
+                             getNodeIndexbyName(graph, nodename)),
+                           label = "ds:hadClimateIndexCalculation")
+    }
+    # Climate index node -------------------------------------------------------
+    descr <- paste("The climate index", ref$description, "is calculated")
+
+    if (!is.na(ref$scale)) {
+        paste0(descr, paste(", considering a scale of", ref$scale, "months"))
+    }
+    ci.nodename <- ref$vocabulary
+    cn <- "ds:ClimateIndex"
+    graph <- my_add_vertices(graph,
+                             name = ci.nodename,
+                             label = index.code,
+                             className = cn,
+                             attr = list("dc:description" = descr))
+    graph <- add_edges(graph,
+                       c(getNodeIndexbyName(graph, nodename),
+                         getNodeIndexbyName(graph, ci.nodename)),
+                       label = "ds:withClimateIndex")
+    # Update temporalResolution ------------------------------------------------
+    timeres.nodename <- paste("TemporalResolution", randomName(), sep = ".")
+    if (ref$time_step != "P1D") {
+        graph <- my_add_vertices(graph,
+                                 name = timeres.nodename,
+                                 label = paste("Update time resolution to", ref$time_step),
+                                 className = "ds:TemporalResolution",
+                                 attr = list("ds:hasTimeStep" = ref$time_step))
+        graph <- add_edges(graph,
+                           c(getNodeIndexbyName(graph, nodename),
+                             getNodeIndexbyName(graph, timeres.nodename)),
+                           label = "ds:hasTemporalResolution")
+    }
+    return(list("graph" = graph, "parentnodename" = nodename))
 }
 
