a6bcea9d9709b55e0daa797152900d02b3574670
jbedia <joaquin.bedia@gmail.com>
2020-02-07 19:07:22 +0100

    suppress automatic check

--- R/metaclipcc.MapProjection.R
+++ R/metaclipcc.MapProjection.R
@@ -6,11 +6,13 @@
 #' @author J Bedia
 
 metaclipcc.MapProjection <- function(proj) {
-    stopifnot(proj %in% knownClassIndividuals(proj, classname = "MapProjection", vocabulary = "graphical_output"))
+    # # stopifnot(proj %in% knownClassIndividuals(proj, classname = "MapProjection",
+    #                                           vocabulary = "graphical_output",
+    #                                           source.vocab = "graphical_output"))
     graph <- make_empty_graph()
     graph <- my_add_vertices(graph,
-                             name = proj.name,
+                             name = proj,
                              label = paste(proj, "Projection"),
                              className = "go:MapProjection")
-    return(list(graph = graph, parentnodename = proj.name))
+    return(list(graph = graph, parentnodename = proj))
 }
