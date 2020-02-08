5691baef936bbca032b6679c649246b8078912e6
jbedia <joaquin.bedia@gmail.com>
2020-02-08 23:11:08 +0100

    Bug fix. Missing vocabulary prefix

--- R/metaclipcc.ColorPalette.R
+++ R/metaclipcc.ColorPalette.R
@@ -15,20 +15,20 @@ metaclipcc.ColorPalette <- function(input.ecv, diverging) {
         cn <- "go:DivergingColorPalette"
         lab <- "Diverging Color Palette"
         if (input.ecv == "pr") {
-            attr.list <- list("hasColorSet" = "#543005,#8c510a,#bf812d,#dfc27d,#f6e8c3,#f5f5f5,#c7eae5,#80cdc1,#35978f,#01665e,#003c30",
+            attr.list <- list("go:hasColorSet" = "#543005,#8c510a,#bf812d,#dfc27d,#f6e8c3,#f5f5f5,#c7eae5,#80cdc1,#35978f,#01665e,#003c30",
                               "ds:referenceURL" = "http://colorbrewer2.org/#type=diverging&scheme=BrBG&n=11")
         } else {
-            attr.list <- list("hasColorSet" = "#67001f,#b2182b,#d6604d,#f4a582,#fddbc7,#f7f7f7,#d1e5f0,#92c5de,#4393c3,#2166ac,#053061",
+            attr.list <- list("go:hasColorSet" = "#67001f,#b2182b,#d6604d,#f4a582,#fddbc7,#f7f7f7,#d1e5f0,#92c5de,#4393c3,#2166ac,#053061",
                               "ds:referenceURL" = "http://colorbrewer2.org/#type=diverging&scheme=RdBu&n=11")
         }
     } else {
         cn <- "go:SequentialColorPalette"
         lab <- "Sequential Color Palette"
         if (input.ecv == "pr") {
-            attr.list <- list("hasColorSet" = "eff3ff,#c6dbef,#9ecae1,#6baed6,#4292c6,#2171b5,#084594",
+            attr.list <- list("go:hasColorSet" = "eff3ff,#c6dbef,#9ecae1,#6baed6,#4292c6,#2171b5,#084594",
                               "ds:referenceURL" = "http://colorbrewer2.org/#type=sequential&scheme=Blues&n=7")
         } else {
-            attr.list <- list("hasColorSet" = "#fee5d9,#fcbba1,#fc9272,#fb6a4a,#ef3b2c,#cb181d,#99000d",
+            attr.list <- list("go:hasColorSet" = "#fee5d9,#fcbba1,#fc9272,#fb6a4a,#ef3b2c,#cb181d,#99000d",
                               "ds:referenceURL" = "http://colorbrewer2.org/#type=sequential&scheme=Reds&n=7")
         }
     }
